dec925c91d67cea903567637b547e9e41f370677
polypixeldev <79737178+polypixeldev@users.noreply.github.com>
2024-10-03 17:45:11 -0400

    Basic page from example

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./pages/App";
 import NotFound from "./pages/NotFound";
 
+import Tampa from "./pages/Tampa";
+
 // To add your own page, uncomment the following import statement.
 // Replace `ExampleCity` with the name of your city.
 // E.g. Ottawa, ON -> `Ottawa`.
@@ -18,6 +20,7 @@ root.render(
       <Routes>
         <Route path="/" element={<App />} />
 
+        <Route path="/tampa" element={<Tampa />} />
         {/*
           To add your own page, copy paste and uncomment the following code! 
           Replace `/example-city` and `ExampleCity` with the name of your city.
