addd03140aab6ed6ecc30bab6df7da6a337d41f0
Glitch752 <xxGlitch752xx@gmail.com>
2024-11-20 19:17:38 -0600

    Oshkosh: Add 4imprint as a sponsor

--- components/Oshkosh/Sponsors.js
+++ components/Oshkosh/Sponsors.js
@@ -5,9 +5,10 @@ export default function Locations() {
         <p className="px-12">Thank you so much to these companies for supporting us!</p>
         <div className="flex flex-wrap flex-row justify-center items-center gap-8 py-10 px-8">
           {/* 40px height, but maintain aspect ratio */}
-          <img className="h-40 object-contain outline outline-pink outline-4 border-8 border-white rounded-md" src="/city/oshkosh/BuffaloWildWingsLogo.png" alt="Buffalo Wild Wings" />
-          <img className="h-40 object-contain outline outline-pink outline-4 border-8 border-white rounded-md" src="/city/oshkosh/SchiekLogo.png" alt="Schiek Sports" />
-          <img className="h-40 object-contain outline outline-pink outline-4 border-8 border-white rounded-md" src="/city/oshkosh/TEKSystemsLogo.jpg" alt="TEK Systems" />
+          <img className="h-40 object-contain outline outline-pink outline-4 border-8 border-white rounded-md bg-white" src="/city/oshkosh/BuffaloWildWingsLogo.png" alt="Buffalo Wild Wings" />
+          <img className="h-40 object-contain outline outline-pink outline-4 border-8 border-white rounded-md bg-white" src="/city/oshkosh/SchiekLogo.png" alt="Schiek Sports" />
+          <img className="h-40 object-contain outline outline-pink outline-4 border-8 border-white rounded-md bg-white" src="/city/oshkosh/TEKSystemsLogo.jpg" alt="TEK Systems" />
+          <img className="h-40 object-contain outline outline-pink outline-4 border-8 border-white rounded-md bg-white" src="/city/oshkosh/4ImprintLogo.png" alt="4imprint" />
         </div>
       </div>
     </div>
