f4a22f653d7c16e0ac3a8300af4e672c82f4b7e3
Sofia Egan <108308587+EerierGosling@users.noreply.github.com>
2024-11-22 22:50:46 -0500

    switching to GET

--- pages/api/v1/waiver.js
+++ pages/api/v1/waiver.js
@@ -1,7 +1,7 @@
 import AirtablePlus from "airtable-plus";
 
 export default async function handler(req, res) {
-  if (req.method !== "POST") {
+  if (req.method !== "GET") {
     return res.status(405).json({
       ok: false,
       error: "Method not allowed",
