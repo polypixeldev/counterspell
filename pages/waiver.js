522e1b5e3768fdababb13edff33e5cc4c56a2cda
Sam Liu <hello@samliu.dev>
2024-11-22 23:58:47 -0500

    updated waiver

--- pages/waiver.js
+++ pages/waiver.js
@@ -1,30 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 export default function Waiver() {
-  const [type, setType] = useState('');
-  const [id, setId] = useState('');
+  const [type, setType] = useState("");
+  const [id, setId] = useState("");
   const [response, setResponse] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleSubmit = async (e) => {
+    setLoading(true);
     e.preventDefault();
 
     const queryParams = new URLSearchParams({ type, id }).toString();
     const res = await fetch(`/api/v1/waiver?${queryParams}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
+      method: "GET",
     });
 
-    const data = await res.json();
+    const data = await res.text();
     setResponse(data);
+    console.log(data);
+    setLoading(false);
   };
 
   return (
-    <>
+    <div className="flex flex-col max-w-xl mx-auto m-12">
       <form onSubmit={handleSubmit} className="p-4 bg-blue-500 text-white">
         <div className="mb-4">
-          <label className="block text-white text-sm font-bold mb-2" htmlFor="type">
+          <label
+            className="block text-white text-sm font-bold mb-2"
+            htmlFor="type"
+          >
             Type
           </label>
           <select
@@ -32,6 +36,7 @@ export default function Waiver() {
             value={type}
             onChange={(e) => setType(e.target.value)}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white"
+            required
           >
             <option value="">Select Type</option>
             <option value="email">Email</option>
@@ -39,7 +44,10 @@ export default function Waiver() {
           </select>
         </div>
         <div className="mb-4">
-          <label className="block text-white text-sm font-bold mb-2" htmlFor="id">
+          <label
+            className="block text-white text-sm font-bold mb-2"
+            htmlFor="id"
+          >
             Email/Name
           </label>
           <input
@@ -48,20 +56,31 @@ export default function Waiver() {
             value={id}
             onChange={(e) => setId(e.target.value)}
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline bg-white"
+            required
           />
         </div>
         <button
           type="submit"
-          className="bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          className="bg-pink bg-blue-700 hover:bg-blue-900 disabled:opacity-50 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          disabled={loading}
         >
           Submit
         </button>
+        <div className="mt-4 text-white">
+          <h2 className="text-lg font-bold">Response:</h2>
+          <pre className="bg-gray-800 p-4 rounded">
+            {loading
+              ? "Loading..."
+              : response === null
+              ? "Submit a request"
+              : response === "true"
+              ? "Waiver submitted"
+              : response === "false"
+              ? "Waiver not submitted"
+              : `Unexpected response ${response}`}
+          </pre>
+        </div>
       </form>
-      <div className="mt-4 text-white">
-        <h2 className="text-lg font-bold">Response:</h2>
-        <pre className="bg-gray-800 p-4 rounded">{JSON.stringify(response, null, 2)}</pre>
-      </div>
-
-    </>
+    </div>
   );
 }
